71393ad7369c1a8feaf57a4e9eb2c0281ac556c8
YOSSEF-WEAL <yweal423@gmail.com>
2025-03-19 22:29:36 +0200

    firest feach

--- src/App-v1.js
+++ src/App-v1.js
@@ -50,17 +50,10 @@ const tempWatchedData = [
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
-const KEY = "2ef73cfc";
-
 export default function App()
 {
-  const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
-
-  // fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
-  fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=${KEY}&s=interstellar`)
-    .then(res => res.json()).then(data => console.log(data.Search));
-
+  const [movies, setMovies] = useState(tempMovieData);
+  const [watched, setWatched] = useState(tempWatchedData);
 
   return (
     <>
