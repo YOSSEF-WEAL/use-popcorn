60c7599a4c788e653d5759c78698f1e2794feeeb
YOSSEF-WEAL <yweal423@gmail.com>
2025-04-04 14:58:10 +0200

    deploy 4

--- src/index.js
+++ src/index.js
@@ -2,29 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-// import StarRating from './StarRating.js';
 
-// function Test()
-// {
-
-//   const [moveRating, setMoveRating] = useState(0);
-
-//   return <div>
-//     <StarRating color='blue' maxRating={10} onSetRating={setMoveRating} />
-//     <p>This movie was rated {moveRating} stars</p>
-//   </div>
-// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    {/* <StarRating maxRating={5} messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']} />
-    <StarRating maxRating={7} size={24} color='red' className='test' defaultRating={3} />
-    <Test /> */}
+
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
