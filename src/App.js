5c3fd4f73658599da54ea317db31a4af972f19c3
YOSSEF-WEAL <yweal423@gmail.com>
2025-04-04 15:34:53 +0200

    add https in movie feach

--- src/App.js
+++ src/App.js
@@ -328,7 +328,7 @@ function MovieDetals({ selectedId, onCloseMovie, onAddWatched, watched })
         setIsLoging(true);
 
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+          `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
         );
         const data = await res.json();
         setMovie(data);
